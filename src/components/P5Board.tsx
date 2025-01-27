c7da30ee66446194d6ed281a6f22068c0af7ace0
loki <fiji8800@gmail.com>
2025-01-27 19:01:40 +0300

    implemented draw pieces

--- src/components/P5Board.tsx
+++ src/components/P5Board.tsx
@@ -1,4 +1,7 @@
 "use client"
+import { store } from '@/store';
+import { P5CanvasInstance } from '@p5-wrapper/react';
+import p5 from 'p5';
 import { useEffect, useRef } from 'react';
 
 // Constants
@@ -17,16 +20,29 @@ export default function P5Board() {
       try {
         const p5 = (await import('p5')).default;
 
-        p5Instance = new p5((p: any) => {
+        p5Instance = new p5((p5: P5CanvasInstance) => {
+          const pieceImages: Record<string, p5.Image> = {};
           let canvasSize = Math.min(window.innerWidth, window.innerHeight);
 
-          p.setup = () => {
-            const canvas = p.createCanvas(canvasSize, canvasSize);
+          p5.preload = () => {
+            const pieces = ['p', 'n', 'b', 'r', 'q', 'k'];
+            const colors = ['w', 'b'];
+            pieces.forEach(piece => {
+              colors.forEach(color => {
+                const fileName = color + piece.toUpperCase();
+                pieceImages[fileName] = p5.loadImage(`/piece/cburnett/${fileName}.svg`);
+              });
+            });
+            //castleImage = p5.loadImage('/images/castle.png');
+          };
+
+          p5.setup = () => {
+            const canvas = p5.createCanvas(canvasSize, canvasSize);
             canvas.parent(renderRef.current);
 
             window.addEventListener('resize', () => {
               canvasSize = Math.min(window.innerWidth, window.innerHeight);
-              p.resizeCanvas(canvasSize, canvasSize);
+              p5.resizeCanvas(canvasSize, canvasSize);
             });
           };
 
@@ -34,19 +50,36 @@ export default function P5Board() {
 
           const drawBoard = () => {
             const tileSize = canvasSize / 8;
-            p.noStroke();
+            p5.noStroke();
 
             for (let y = 0; y < 8; y++) {
               for (let x = 0; x < 8; x++) {
-                p.fill(isLightSquare(x, y) ? LIGHT_TILE : DARK_TILE);
-                p.rect(x * tileSize, y * tileSize, tileSize, tileSize);
+                p5.fill(isLightSquare(x, y) ? LIGHT_TILE : DARK_TILE);
+                p5.rect(x * tileSize, y * tileSize, tileSize, tileSize);
               }
             }
           };
 
-          p.draw = () => {
-            p.background(250);
+          const drawPieces = () => {
+            const tileSize = canvasSize / 8;
+            store.localChess.board().forEach((row, y) => {
+              row.forEach((piece, x) => {
+                if (piece) {
+                  const fileName = piece.color + piece.type.toUpperCase();
+                  const img = pieceImages[fileName];
+                  if (img) {
+                    let drawX = x, drawY = y;
+                    p5.image(img, drawX * tileSize, drawY * tileSize, tileSize, tileSize);
+                  }
+                }
+              });
+            });
+          };
+
+          p5.draw = () => {
+            p5.background(250);
             drawBoard();
+            drawPieces();
           };
         }, renderRef.current);
       } catch (error) {
